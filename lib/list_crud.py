6f8697b4ef5e4924438d74fa6504b9c157f51792
Kelvin okumba <okumbakelvin@gmail.com>
2023-10-10 13:58:14 +0300

    done

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,29 @@
 def create_an_empty_list():
-    return None
+    return []
 
-def create_a_list():
-    return None
+def create_four_element_list():
+    # Create a list with four elements
+    my_list = list(['apple', 42, True, 3.14])
+    return my_list
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l.append(element)
+    
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l.insert(0, element)
 
 def remove_element_from_end_of_list(l):
-    return None
+    l.pop(l)
 
 def remove_element_from_start_of_list(l):
-    return None
+    l.pop(0)
 
 def retrieve_first_element_from_list(l):
-    return None
+    return l[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+    return l[index]
 
 def retrieve_last_element_from_list(l):
-    return None
+    return l[-1]
